cbda6c23f47e6e73e504daa10c52c15a6b798948
Joao Costa <joao.costa@codacy.com>
2019-11-14 15:44:21 +0000

    fix: codacy proposed fixes

--- .circleci/validateDeploymentStatus.sh
+++ .circleci/validateDeploymentStatus.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-doctl kubernetes cluster kubeconfig save $DOKS_CLUSTER_NAME --set-current-context
+doctl kubernetes cluster kubeconfig save "$DOKS_CLUSTER_NAME" --set-current-context
 DEPLOYMENTS=$(kubectl get deployments -n codacy | awk '{print "deployment/"$1}' | tail -n +2 )
 for DEPLOYMENT in ${DEPLOYMENTS[@]}
 do
-    kubectl rollout status -n codacy --watch $DEPLOYMENT
+    kubectl rollout status -n codacy --watch "$DEPLOYMENT"
 done
