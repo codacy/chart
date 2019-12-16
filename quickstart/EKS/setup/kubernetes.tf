717f806828a53ee3529f0937e7253da1a2f11c99
Paulo Martins <paulo.pontes.m@gmail.com>
2019-12-16 18:43:12 +0000

    Deployed 9d544eb with MkDocs version: 1.0.4

---
+++ quickstart/EKS/setup/kubernetes.tf
@@ -0,0 +1,53 @@
+# kubernetes.tf - setup kubernetes for codacy
+
+# this allows worker nodes to join the cluster. See https://docs.aws.amazon.com/eks/latest/userguide/add-user-role.html
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = <<-YAML
+    - rolearn: ${data.aws_iam_role.worker.arn}
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+    YAML
+  }
+}
+
+# CRDs for certificate-manager. See https://github.com/jetstack/cert-manager
+resource "null_resource" "cert_manager_crds" {
+  triggers = {
+    manifest_sha1 = sha1(data.template_file.cert_manager_crds.rendered)
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl apply -f -<<EOF && kubectl label namespace --overwrite kube-system certmanager.k8s.io/disable-validation='true'\n${data.template_file.cert_manager_crds.rendered}\nEOF"
+  }
+}
+
+# create image pull secret in main namespace
+resource "kubernetes_namespace" "codacy" {
+  metadata {
+    name = var.main_namespace
+  }
+}
+
+resource "kubernetes_secret" "docker_credentials" {
+  metadata {
+    name = "docker-credentials"
+    namespace = var.main_namespace
+  }
+  data = {
+    ".dockerconfigjson" = "{\"auths\": {\"https://index.docker.io/v1/\": {\"auth\": \"${base64encode("${var.docker_username}:${var.docker_password}")}\"}}}"
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+
+  depends_on = [
+    kubernetes_namespace.codacy
+  ]
+}
