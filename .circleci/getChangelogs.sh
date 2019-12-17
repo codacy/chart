9c21ecbb5158bf5c85ec8d598ba595f289ad0dae
Joao Costa <joao.costa@codacy.com>
2019-12-17 09:39:40 +0000

    bump: deliberately change artifact versions for changelog debugging
    fix: prevent rsa fingerprint prompt
    
    fix: quotes preventing checkout have been removed
    
    refactor: revert bump of versions. revert test workflow

--- .circleci/getChangelogs.sh
+++ .circleci/getChangelogs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 REQUIREMENTS_FILE="codacy/requirements.yaml"
 OLD_LOCK_FILE="codacy/requirements_old.lock"
 NEW_LOCK_FILE="codacy/requirements.lock"
@@ -21,11 +22,10 @@ function cloneRepository() {
     CHANGELOG_FILENAME="$PROJECT_NAME-changelog.txt"
     JIRA_CHANGELOG_FILENAME="$PROJECT_NAME-jira.txt"
 
-    
     if [ "$REPOSITORY_URL" != "null" ];
         then
             echo "[OK] $PROJECT_NAME($REPOSITORY_URL) -> $OLD_VERSION : $NEW_VERSION"
-            git clone "$REPOSITORY_URL $PROJECT_NAME" --quiet
+            git clone "$REPOSITORY_URL" "$PROJECT_NAME" --quiet
             cd "$PROJECT_NAME"
             getChangelog "$OLD_VERSION" "$NEW_VERSION" "$CHANGELOG_FILENAME" "$JIRA_CHANGELOG_FILENAME";
             cd ..
@@ -33,7 +33,6 @@ function cloneRepository() {
         else
             echo "[Skipped] $PROJECT_NAME has no repository url."
     fi
-    
 }
 rm -rf ./changelogs
 mkdir changelogs
@@ -41,7 +40,7 @@ for key in $DEPENDENCIES
 do
     OLD_VERSION=$(yq r $OLD_LOCK_FILE dependencies -j | jq ".[] | select(.name==\"$key\").version" | sed "s/\"//g")
     NEW_VERSION=$(yq r $NEW_LOCK_FILE dependencies -j | jq ".[] | select(.name==\"$key\").version" | sed "s/\"//g")
-    [ "$OLD_VERSION" != "$NEW_VERSION" ] && cloneRepository "$key" "$OLD_VERSION" "$NEW_VERSION" 
+    [ "$OLD_VERSION" != "$NEW_VERSION" ] && cloneRepository "$key" "$OLD_VERSION" "$NEW_VERSION"
 done
 
 count=$(find ./changelogs -maxdepth 1 -type f | wc -l | awk '{print $1}')
@@ -52,6 +51,5 @@ then
 else
     echo "$((count / 2)) component(s) changed. "
 fi
-
 
 exit 0
