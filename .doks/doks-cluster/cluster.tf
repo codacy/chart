b6f3499d8ee065d9c2519b6655f59fd7d0dabcbc
Joao Costa <joao.costa@codacy.com>
2020-02-27 14:02:21 +0000

    fix: change node types

--- .doks/doks-cluster/cluster.tf
+++ .doks/doks-cluster/cluster.tf
@@ -7,9 +7,9 @@ resource "digitalocean_kubernetes_cluster" "codacy_k8s" {
     name       = "codacy-doks-pool-${terraform.workspace}"
     size       = "s-2vcpu-2gb"
     auto_scale = true
+    node_count = 1
     min_nodes = 0
     max_nodes = 0
-    node_count = 1
   }
 
   provisioner "local-exec" {
@@ -19,7 +19,7 @@ resource "digitalocean_kubernetes_cluster" "codacy_k8s" {
 
 resource "digitalocean_kubernetes_node_pool" "auto-scale-pool-01" {
   cluster_id = digitalocean_kubernetes_cluster.codacy_k8s.id
-  name       = "codacy-doks-pool-${terraform.workspace}-auto-scale-pool"
+  name       = "codacy-doks-pool-${terraform.workspace}-auto-scale-pool-01"
   size       = var.node_type
   auto_scale = true
   min_nodes = 2
