275e600fc1ed0fbf8a7fa45e07e92b5b56bc6ac1
André Meira <andre.meira@codacy.com>
2020-03-19 18:53:26 +0000

    docs: Add possibility to extract a certain number of days of logs

--- docs/configuration/extract-codacy-logs.sh
+++ docs/configuration/extract-codacy-logs.sh
@@ -4,7 +4,9 @@
 
 usage()
 {
-    echo "Usage: $0 -n <NAMESPACE> "
+    echo "Usage: $0 -n <NAMESPACE> [-d <DAYS_OF_LOGS>]"
+    echo "When using the -d flag, <DAYS_OF_LOGS> must be between 1 and 7"
+    echo "Example: '$0 -n codacy -d 3' would retrieve the last 3 days of logs from the 'codacy' namespace of the kubernetes cluster"
     exit 3
 }
 
@@ -18,10 +20,17 @@ trap cleanup EXIT
 
 #### Log extraction script ####
 
-while getopts "n:" option; do
+while getopts "n:d:" option; do
     case ${option} in
         n )
             NAMESPACE=${OPTARG}
+            ;;
+        d )
+            if [ $OPTARG -le 0 ] || [ $OPTARG -ge 8 ]; then
+                usage
+            else
+                DAYS=$((${OPTARG}-1)) # Day 0 is today
+            fi
             ;;
         \? )
             usage
@@ -36,19 +45,30 @@ LOGS_DIR="$(mktemp -d)/codacy_logs_$CURRENT_DATE_TIME"
 
 echo "Starting log files extraction"
 
-# Check if kubectl is available
+# Check if zip is available
+echo "Checking if zip is installed..."
+if ! zip --version &>/dev/null; then
+    echo "zip not installed"
+    echo "Please install zip"
+    echo "Log files extraction failed, exiting..."
+    exit 4
+fi
+
+
+# Check if kubectl or microk8s.kubectl is available
 echo "Checking if kubectl is installed..."
-if ! kubectl version &>/dev/null; then
+KUBECTL=$(which kubectl || which microk8s.kubectl)
+if [ $? -ne 0 ]; then
     echo "kubectl not installed"
     echo "Please install kubectl version specified in Codacy's documentation (see here - https://codacy.github.io/chart/#preparing-to-install-codacy) or the version used when installing your cluster"
-    echo "To install kubectl see https://kubernetes.io/docs/tasks/tools/install-kubectl/"
+    echo "To install kubectl see https://kubernetes.io/docs/tasks/tools/install-kubectl/ (or https://microk8s.io/docs/ if you are running a microk8s kubernetes cluster)"
     echo "Log files extraction failed, exiting..."
     exit 4
 fi
 
 # Check current cluster context
 echo "Checking access to kubernetes cluster..."
-KUBE_CTX=$(kubectl config current-context)
+KUBE_CTX=$($KUBECTL config current-context)
 if [ $? -ne 0 ]; then
     echo "No kubernetes cluster context configured"
     echo "Log files extraction failed, exiting..."
@@ -71,7 +91,7 @@ fi
 
 # Get pod name for the logs pod
 echo "Checking if the logs kubernetes pod exists..."
-LOGS_POD_NAME=$(kubectl get pods -n $NAMESPACE -l app=minio -o jsonpath='{.items[*].metadata.name}')
+LOGS_POD_NAME=$($KUBECTL get pods -n $NAMESPACE -l app=minio -o jsonpath='{.items[*].metadata.name}')
 if [ $? -ne 0 ]; then
     echo "Failed to get the name of the logs kubernetes pod, for namespace $NAMESPACE"
     echo "Are you sure you are in the right kubernetes cluster context?"
@@ -81,20 +101,43 @@ fi
 
 # Copy logs to local filesystem
 echo "Extracting log files..."
-if ! kubectl cp $NAMESPACE/$LOGS_POD_NAME:/export/logs $LOGS_DIR; then
+if ! $KUBECTL cp $NAMESPACE/$LOGS_POD_NAME:/export/logs $LOGS_DIR; then
     echo "Failed to extract log files from kubernetes pod $LOGS_POD_NAME to local directory $LOGS_DIR"
     echo "Are you sure you are in the right kubernetes cluster context?"
     echo "Log files extraction failed, exiting..."
     exit 9
 fi
 
-# Compress logs in ZIP file (-9 is maximum, slowest, compression)
+
 echo "Compressing extracted log files..."
-if ! zip -r9 codacy_logs_$CURRENT_DATE_TIME.zip $LOGS_DIR; then
-    echo "Failed to compress logs (located in $LOGS_DIR) to a ZIP file"
-    echo "If this step continues to fail, you can compress the files manually"
-    echo "Log files compression failed, exiting..." 
-    exit 10
+if [ -n "$DAYS" ]; then
+
+    # Generate a range of dates to make pattern matching easier
+    while [ $DAYS -ge 0 ]; do
+        LOGS_DATE=$(date -v -$DAYS\d "+%Y-%m-%d")
+
+        # Find all log files that match the pattern and add them to the ZIP archive (-9 is maximum, slowest, compression)
+        find $LOGS_DIR -iname $LOGS_DATE\* -type f | xargs -L 10 zip -ur9 codacy_logs_$CURRENT_DATE_TIME.zip
+
+        # Incremental zipping might fail
+        if [ $? -ne 0 ]; then
+            echo "Failed to compress logs (located in $LOGS_DIR) to a ZIP file"
+            echo "If this step continues to fail, you can compress the files manually"
+            echo "Log files compression failed, exiting..."
+            exit 10
+        fi
+
+        DAYS=$(($DAYS - 1))
+    done
+
+else
+    # Compress logs in ZIP file (-9 is maximum, slowest, compression)
+    if ! zip -r9 codacy_logs_$CURRENT_DATE_TIME.zip $LOGS_DIR; then
+        echo "Failed to compress logs (located in $LOGS_DIR) to a ZIP file"
+        echo "If this step continues to fail, you can compress the files manually"
+        echo "Log files compression failed, exiting..."
+        exit 10
+    fi
 fi
 
 echo "Log file extraction completed"
