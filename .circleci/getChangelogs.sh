5c894bf7d9b8192e63948471682cf907fc16ac0b
joaocosta-codacy <56255055+joaocosta-codacy@users.noreply.github.com>
2020-04-24 11:49:27 +0100

    fix: get changelog for new dependencies (#343)
    * fix: use latest tag instead of stable
    
    * refactor: changed messages text
    
    * fix: broken methods reverted
    
    * fix: adding verboseness

--- .circleci/getChangelogs.sh
+++ .circleci/getChangelogs.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 set -e
-REQUIREMENTS_FILE="codacy/requirements.yaml"
-OLD_LOCK_FILE="codacy/requirements_old.lock"
-NEW_LOCK_FILE="codacy/requirements.lock"
+REQUIREMENTS_FILE="$(pwd)/codacy/requirements.yaml"
+OLD_LOCK_FILE="$(pwd)/codacy/requirements_old.lock"
+NEW_LOCK_FILE="$(pwd)/codacy/requirements.lock"
+CHANGELOG_FILE="$(pwd)/changelogs/changelog.md"
 
-function prepareChangelogMarkdown(){
+function appendToChangelog() {
+    echo "$1" >> "$CHANGELOG_FILE"
+}
+
+function prepareEnvironment() {
+    rm -rf ./changelogs
+    mkdir changelogs
+    prepareChangelogMarkdown
+    git cat-file blob "latest":"codacy/requirements.lock" > "$OLD_LOCK_FILE"
+}
+
+function prepareChangelogMarkdown() {
     changelog_conf_path=$(realpath .)/.changelogs
     export GITCHANGELOG_CONFIG_FILENAME=$changelog_conf_path/gitchangelog.rc
     markdown_template_path=$(echo "$changelog_conf_path/markdown.tpl" | sed 's/\//\\\//g')
     sed "s/##PATHMACRO##/$markdown_template_path/g" "$changelog_conf_path/gitchangelogtemplate.rc" >> "$GITCHANGELOG_CONFIG_FILENAME"
-    echo "# Codacy Chart Changelog" >> "changelogs/changelog.md"
+    appendToChangelog "# Codacy Chart Changelog"
 }
 
 function getChangelog() {
@@ -17,12 +29,13 @@ function getChangelog() {
     old_version=$2
     new_version=$3
     repository_url=$4
-    echo " * getting changelog ..."
     cd "$project_name"
+    echo "Getting changelog: $old_version -> $new_version"
     git fetch --all --quiet
     git checkout tags/"$old_version" --quiet
-    echo "## $project_name($repository_url)" >> "../changelogs/changelog.md"
-    gitchangelog "$old_version" "$new_version" >> "../changelogs/changelog.md"
+    appendToChangelog "## $project_name([$repository_url]())"
+    changeset="$(gitchangelog $old_version $new_version)"
+    appendToChangelog "$changeset"
     cd ..
     rm -rf "./$project_name"
 }
@@ -32,31 +45,37 @@ function cloneRepository() {
     old_version=$2
     new_version=$3
     repository_url=$4
-    echo "[OK] $project_name($repository_url) : $old_version -> $new_version"
+    echo "Checking out: $project_name($repository_url)"
     git clone "$repository_url" "$project_name" --quiet
 }
 
-rm -rf ./changelogs
-mkdir changelogs
-prepareChangelogMarkdown
+function handleNewDependency() {
+    echo "New dependency $1($2): $3"
+    appendToChangelog "" # add blank line in changelog.md
+    appendToChangelog "## $1([$2]())"
+    appendToChangelog "### $3"
+    appendToChangelog "* This dependency was introduced at this version."
+    appendToChangelog "" # add blank line in changelog.md
+}
 
-LATEST_TAG="stable"
-git cat-file blob "$LATEST_TAG":"$NEW_LOCK_FILE" > "$OLD_LOCK_FILE"
-DEPENDENCIES=$(yq r $OLD_LOCK_FILE dependencies -j | jq ".[].name" | sed "s/\"//g")
-
-for key in $DEPENDENCIES
+echo "Getting changelogs..."
+prepareEnvironment
+echo "Resolving dependencies..."
+dependencies=$(yq r "$NEW_LOCK_FILE" dependencies -j | jq -r ".[].name")
+for dependency in $dependencies
 do
-    old_version=$(yq r $OLD_LOCK_FILE dependencies -j | jq ".[] | select(.name==\"$key\").version" | sed "s/\"//g")
-    new_version=$(yq r $NEW_LOCK_FILE dependencies -j | jq ".[] | select(.name==\"$key\").version" | sed "s/\"//g")
-    repository_url=$(yq r $REQUIREMENTS_FILE dependencies -j | jq ".[] | select(.name==\"$key\").git" | sed "s/\"//g")
-    if [ "$old_version" != "$new_version" ] && [ "$repository_url" != "null" ];
+    old_version=$(yq r $OLD_LOCK_FILE dependencies -j | jq -r ".[] | select(.name==\"$dependency\").version")
+    new_version=$(yq r $NEW_LOCK_FILE dependencies -j | jq -r ".[] | select(.name==\"$dependency\").version")
+    repository_url=$(yq r $REQUIREMENTS_FILE dependencies -j | jq -r ".[] | select(.name==\"$dependency\").git")
+
+    if [ "$old_version" != "" ] && [ "$old_version" != "$new_version" ] && [ "$repository_url" != "null" ];
         then
-            cloneRepository "$key" "$old_version" "$new_version" "$repository_url"
-            getChangelog "$key" "$old_version" "$new_version" "$repository_url"
+            cloneRepository "$dependency" "$old_version" "$new_version" "$repository_url"
+            getChangelog "$dependency" "$old_version" "$new_version" "$repository_url"
         else
-            echo "[Skipped] $key:"
-            [ "$repository_url" == "null" ] && echo "  * has no repository url."
-            [ "$old_version" == "$new_version" ] && echo "  * $old_version is the same as $new_version"
+            [ "$repository_url" == "null" ] && echo "Skipped $dependency: has no repository url."
+            [ "$old_version" == "$new_version" ] && echo "Skipped $dependency: $old_version is the same as $new_version"
+            [ "$old_version" == "" ] && handleNewDependency $dependency $repository_url $new_version
     fi
 done
 
