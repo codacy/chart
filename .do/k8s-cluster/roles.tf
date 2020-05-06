4dd08a633a5beee2d3375518f30a855c0a4751ae
Joao Machado <machado@codacy.com>
2020-05-06 15:52:14 +0100

    clean: Remove unnecessary Tiller resources

--- .do/k8s-cluster/roles.tf
+++ .do/k8s-cluster/roles.tf
@@ -25,31 +25,3 @@ resource "kubernetes_cluster_role_binding" "admin" {
     api_group = "rbac.authorization.k8s.io"
   }
 }
-
-# create service account for tiller
-resource "kubernetes_service_account" "tiller" {
-  metadata {
-    name      = "tiller"
-    namespace = "kube-system"
-  }
-
-  automount_service_account_token = true
-}
-
-resource "kubernetes_cluster_role_binding" "tiller" {
-  metadata {
-    name = "tiller-binding"
-  }
-
-  subject {
-    kind      = "ServiceAccount"
-    name      = "tiller"
-    namespace = "kube-system"
-  }
-
-  role_ref {
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-    api_group = "rbac.authorization.k8s.io"
-  }
-}
