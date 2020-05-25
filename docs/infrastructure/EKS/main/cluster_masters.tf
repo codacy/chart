959848fea4d62e0ace0c55619b7a0ec499f0b1a9
Paulo Martins <paulo.pontes.m@gmail.com>
2020-05-25 18:04:20 +0100

    fix: aws_awth for eks

--- docs/infrastructure/EKS/main/cluster_masters.tf
+++ docs/infrastructure/EKS/main/cluster_masters.tf
@@ -30,29 +30,6 @@ resource "aws_eks_cluster" "main" {
 
 }
 
-# Get an authentication token to communicate with an EKS cluster, using the "kubernetes" provider.
-resource "kubernetes_config_map" "aws_auth" {
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
-  }
-
-  data = {
-    mapRoles = <<-YAML
-    - rolearn: ${aws_iam_role.eks_master.arn}
-      username: system:node:{{EC2PrivateDNSName}}
-      groups:
-        - system:bootstrappers
-        - system:nodes
-    YAML
-  }
-  depends_on = [
-    aws_eks_cluster.main
-  ]
-}
-
-
-
 ### security group
 resource "aws_security_group" "eks_master" {
   name        = "${var.project_slug}-cluster-master"
