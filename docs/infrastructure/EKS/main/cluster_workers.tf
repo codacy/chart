9f3fb26905bcef374c7184abac8549e2c8365301
Paulo Martins <paulo.pontes.m@gmail.com>
2020-05-25 18:07:34 +0100

    fix: aws_awth for eks (#464)

--- docs/infrastructure/EKS/main/cluster_workers.tf
+++ docs/infrastructure/EKS/main/cluster_workers.tf
@@ -68,6 +68,26 @@ resource "aws_launch_configuration" "workers" {
   ]
 }
 
+# Get an authentication token to communicate with an EKS cluster, using the "kubernetes" provider.
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = <<-YAML
+    - rolearn: ${aws_iam_role.eks_worker.arn}
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+    YAML
+  }
+  depends_on = [
+    aws_eks_cluster.main
+  ]
+}
 
 ### join nodes to the cluster
 locals {
