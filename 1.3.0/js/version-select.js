de57c9028952607fc6352a7dcfb3e0f69e4729d5
Paulo Ribeiro <paulo.ribeiro@codacy.com>
2020-06-26 15:49:09 +0000

    Deployed 527b2e1 to 1.3.0 with MkDocs 1.1.2 and mike 0.5.1

--- 1.3.0/js/version-select.js
+++ 1.3.0/js/version-select.js
@@ -1,19 +1,18 @@
-window.addEventListener("DOMContentLoaded", function() {
-  // This is a bit hacky. Figure out the base URL from a known CSS file the
-  // template refers to...
-  var ex = new RegExp("/?css/version-select.css");
-  var sheet = document.querySelector('link[href$="version-select.css"]');
-
-  var ABS_BASE_URL = sheet.href.replace(ex, "");
-  var CURRENT_VERSION = ABS_BASE_URL.split("/").pop();
+window.addEventListener("DOMContentLoaded", function () {
+  var VERSION = window.location.pathname.split("/")[1];
 
   function makeSelect(options, selected) {
     var select = document.createElement("select");
     select.classList.add("form-control");
+    select.classList.add("select-text");
 
-    options.forEach(function(i) {
-      var option = new Option(i.text, i.value, undefined,
-                              i.value === selected);
+    options.forEach(function (i) {
+      var option = new Option(
+        i.text,
+        "/" + i.value,
+        undefined,
+        i.value === selected
+      );
       select.add(option);
     });
 
@@ -21,26 +20,40 @@ window.addEventListener("DOMContentLoaded", function() {
   }
 
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", ABS_BASE_URL + "/../versions.json");
-  xhr.onload = function() {
+  // Obtain JSON listing all available versions
+  xhr.open("GET", window.location.origin + "/versions.json");
+  xhr.onload = function () {
     var versions = JSON.parse(this.responseText);
 
-    var realVersion = versions.find(function(i) {
-      return i.version === CURRENT_VERSION ||
-             i.aliases.includes(CURRENT_VERSION);
-    }).version;
+    // Identify which is the current version
+    var currentVersion = versions.find(function (i) {
+      return i.version === VERSION || i.aliases.includes(VERSION);
+    });
 
-    var select = makeSelect(versions.map(function(i) {
-      return {text: i.title, value: i.version};
-    }), realVersion);
-    select.addEventListener("change", function(event) {
-      window.location.href = ABS_BASE_URL + "/../" + this.value;
+    var select = makeSelect(
+      versions.map(function (i) {
+        return { text: i.title, value: i.version };
+      }),
+      currentVersion
+    );
+
+    select.addEventListener("change", function (event) {
+      window.location.href = window.location.origin + this.value;
     });
 
     var container = document.createElement("div");
     container.id = "version-selector";
-    container.className = "md-nav__item";
+    container.classList.add("md-nav__item");
+    container.classList.add("select");
     container.appendChild(select);
+
+    var spanHighlight = document.createElement("span");
+    spanHighlight.classList.add("select-highlight");
+    container.appendChild(spanHighlight);
+
+    var spanBar = document.createElement("span");
+    spanBar.classList.add("select-bar");
+    container.appendChild(spanBar);
 
     var sidebar = document.querySelector(".md-nav--primary > .md-nav__list");
     sidebar.parentNode.insertBefore(container, sidebar);
