e3ff9fe425aae93a9236ea87ca8e1b0e1c08488f
Hélio Rocha <hjrocha@gmail.com>
2023-10-25 11:44:59 +0100

    release: prepare 13.0.0

--- .do/k8s-cluster/variables.tf
+++ .do/k8s-cluster/variables.tf
@@ -5,7 +5,7 @@
 variable "k8s_version" {
   description = "Kubernetes version. See available versions with `doctl kubernetes options versions`"
   type        = string
-  default     = "1.25.8-do.0"
+  default     = "1.26.9-do.0"
 }
 
 variable "k8s_kubeconfig" {
